9644aa1911432a37f2e2facb70733d7c386d740b
Carson Pepe <carsonpepe@gmail.com>
2022-05-13 12:44:46 -0700

    changed '===' to '=='

--- todo-app-frontend/src/Center-body/CenterBody.js
+++ todo-app-frontend/src/Center-body/CenterBody.js
@@ -15,16 +15,16 @@ function CenterBody(props){
 
     function getView(currentState){
 
-        if(currentState === SETTINGS_VIEW) {
+        if(currentState == SETTINGS_VIEW) {
             return <div><p>0</p></div>;
-        } else if(currentState === ADD_VIEW) {
+        } else if(currentState == ADD_VIEW) {
             // Need to add props (handleSubmit={function}) to NewTodo that comply with backend
             return (
                 <div className="container">
                     <NewTodo />
                 </div>
             );
-        } else if(currentState === PLANNER_VIEW) {
+        } else if(currentState == PLANNER_VIEW) {
             return <div><p>2</p></div>;
         } else {
             return <div><p>3</p></div>;
