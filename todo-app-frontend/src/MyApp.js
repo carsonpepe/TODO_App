07242bc7d9f4630cd3a5705edd252488678a2d01
Zack Soucek <zacksoucek@gmail.com>
2022-05-23 11:30:41 -0700

    Added code from tut to frontend, currently no button

--- todo-app-frontend/src/MyApp.js
+++ todo-app-frontend/src/MyApp.js
@@ -5,8 +5,34 @@ import Leftbar from './Leftbar/Leftbar';
 import Rightbar from './Rightbar/Rightbar';
 import './MyApp.css'
 
+var indexRouter = require('./routes/index');
+var authRouter = require('./routes/auth');
+var logger = require('morgan');
+var session = require('express-session');
+var passport = require('passport');
+
+var SQLiteStore = require('connect-sqlite3')(session);
+
 
 const PLANNER_VIEW_TYPE = 2;
+var app = express();
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+  store: new SQLiteStore({ db: 'sessions.db', dir: './var/db' })
+}));
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    store: new SQLiteStore({ db: 'sessions.db', dir: './var/db' })
+  }));
+  app.use(passport.authenticate('session'));
 
 
 function MyApp(){
@@ -17,13 +43,13 @@ function MyApp(){
             categoryType: null,
         }
      );
-    
+
     function changeViewState(centerView){
         setView(
             {viewType: centerView.viewType, categoryType: centerView.categoryType}
         );
     }
-    
+
 
 
     return (
