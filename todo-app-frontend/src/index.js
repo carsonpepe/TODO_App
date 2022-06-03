0228f8357ea088e79b26e984c973aac16783a553
Carson Pepe <carsonpepe@gmail.com>
2022-06-02 22:58:45 -0700

    fixed index.js to use MyApp again

--- todo-app-frontend/src/index.js
+++ todo-app-frontend/src/index.js
@@ -5,4 +5,4 @@ import Page from './Page'
 import './index.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
-ReactDOM.render(<div><Page/></div>, document.getElementById('root'));
+ReactDOM.render(<div className="box"><MyApp /></div>, document.getElementById('root'));
