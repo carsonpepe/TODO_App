7711a8f0f01bfdfdbf564b323e339a032a5505b4
pangling4 <philipang515@gmail.com>
2022-06-03 22:26:48 -0700

    Update services.js

--- todo-app-backend/models/services.js
+++ todo-app-backend/models/services.js
@@ -34,9 +34,6 @@ async function addUser(user) {
         const userExists = await getUserByUsername(user.name);
         console.log('finished getuserByusername successfully'+ userExists);
         if (userExists) {
-            //this means the suers is already int he database, not a new user
-            // console.log('user exists');
-
             return false;
         }
         else {
@@ -79,34 +76,11 @@ async function addCategory(id, category) {
     }
 }
 
-// returns a user based on the provided username
-// async function getUserByUsername(username){
-//     console.log(username);
-//     try {
-//         userModel.findOne({"name": username}, await function (err, docs) {
-//                 if (err) {
-//                     console.log("2" + err);
-//                     return false;
-//                 } else {
-//                     console.log("find() didn't fail");
-//                     //console.log(docs);
-//                     console.log("docs hit");
-//                     return docs;
-//                 }
-//             });
-//     }
-//     catch (error) {
-//         console.log("3"+ error);
-//         return false;
-//     }
-// }
-
 
 async function findUserByName(username){
     return await userModel.find({"name": username});
 }
 
-//use this in backend
 async function getUserByUsername(username){
     let result;
     if(username === undefined){
@@ -136,34 +110,30 @@ async function getTodos(id){
     // console.log(currentUser._id);
     foundTodos = currentUser.todoItems;
     if (!foundTodos) {
-        console.log(foundTodos);
+        // console.log(foundTodos);
         return [];
     }
     return foundTodos;
 }
 
 async function getCategories(id){
-    console.log(id);
+    // console.log(id);
     const currentUser = await findUserById(id);
-    console.log(currentUser._id);
+    // console.log(currentUser._id);
     foundCategories = currentUser.categories;
     if (!foundCategories) {
-        console.log(foundCategories);
+        // console.log(foundCategories);
         return [];
     }
     return foundCategories;
-    
+
 }
 
-async function getUserSettings(id){
-    const currentUser = await findUserById(id);
-    return currentUser.settings;
-}
+// async function getUserSettings(id){
+//     const currentUser = await findUserById(id);
+//     return currentUser.settings;
+// }
 
-async function getUserCategories(id){
-    const currentUser = await findUserById(id);
-    return currentUser.categories;
-}
 
 
 async function findTodosByCategory(id, category_name) {
@@ -173,80 +143,79 @@ async function findTodosByCategory(id, category_name) {
     return savedTodo;
 }
 
-async function findTodosByCompleted(id, complete_val) {
-    const currentUser = findUserById(id);
-    currentUser.todoItems.find({completed: complete_val});
-    const savedTodos = await currentUser.save();
-    return savedTodos;
-}
-
-async function markCompleted(id, todo_id){
-    const currentUser = findUserById(id);
-    currentUser.todoItems.findByIdAndUpdate(todo_id, {completed: true});
-    const savedTodo = await currentUser.save();
-    return savedTodo;
-}
-
-async function markUncomplete(id, todo_id){
-    const currentUser = findUserById(id);
-    currentUser.todoItems.findByIdAndUpdate(todo_id, {completed: false});
-    const savedTodo = await currentUser.save();
-    return savedTodo;
-}
-
-
-// async function removeCompleted(id){
+// async function findTodosByCompleted(id, complete_val) {
 //     const currentUser = findUserById(id);
-//     // return await currentUser.todoItems.deleteMany({completed: { $eq: true}});
-//     currentUser.todoItems.deleteMany({completed: { $eq: true}});
+//     currentUser.todoItems.find({completed: complete_val});
+//     const savedTodos = await currentUser.save();
+//     return savedTodos;
+// }
+
+// async function markCompleted(id, todo_id){
+//     const currentUser = findUserById(id);
+//     currentUser.todoItems.findByIdAndUpdate(todo_id, {completed: true});
 //     const savedTodo = await currentUser.save();
 //     return savedTodo;
 // }
 
-// async function changeDescription(id, todo_id, new_description){
+// async function markUncomplete(id, todo_id){
 //     const currentUser = findUserById(id);
-//     currentUser.findByIdAndUpdate(todo_id, {description: new_description});
+//     currentUser.todoItems.findByIdAndUpdate(todo_id, {completed: false});
 //     const savedTodo = await currentUser.save();
 //     return savedTodo;
 // }
 
-// async function changeTitle(id, todo_id, new_title){
-//     const currentUser = findUserById(id);
-//     currentUser.findByIdAndUpdate(todo_id, {title: new_title});
-//     const savedTodo = await currentUser.save();
-//     return savedTodo;
-// }
+// // async function removeCompleted(id){
+// //     const currentUser = findUserById(id);
+// //     // return await currentUser.todoItems.deleteMany({completed: { $eq: true}});
+// //     currentUser.todoItems.deleteMany({completed: { $eq: true}});
+// //     const savedTodo = await currentUser.save();
+// //     return savedTodo;
+// // }
 
-// async function turnNotificationsOn(id,todo_id){
-//     const currentUser = findUserById(id);
-//     currentUser.todoItems.findByIdAndUpdate(todo_id, {notificationToggle: true});
-//     const savedTodo = await currentUser.save();
-//     return savedTodo;
-// }
+// // async function changeDescription(id, todo_id, new_description){
+// //     const currentUser = findUserById(id);
+// //     currentUser.findByIdAndUpdate(todo_id, {description: new_description});
+// //     const savedTodo = await currentUser.save();
+// //     return savedTodo;
+// // }
 
-// async function turnNotificationsOff(id,todo_id){
-//     const currentUser = findUserById(id);
-//     currentUser.todoItems.findByIdAndUpdate(todo_id, {notificationToggle: false});
-//     const savedTodo = await currentUser.save();
-//     return savedTodo;
-// }
+// // async function changeTitle(id, todo_id, new_title){
+// //     const currentUser = findUserById(id);
+// //     currentUser.findByIdAndUpdate(todo_id, {title: new_title});
+// //     const savedTodo = await currentUser.save();
+// //     return savedTodo;
+// // }
 
-// async function changeCategory(id, todo_id, new_category){
-//     // maybe a check to see if the category exists?
-//     const currentUser = findUserById(id);
-//     currentUser.findByIdAndUpdate(todo_id, {category: new_category});
-//     const savedTodo = await currentUser.save();
-//     return savedTodo;
-// }
+// // async function turnNotificationsOn(id,todo_id){
+// //     const currentUser = findUserById(id);
+// //     currentUser.todoItems.findByIdAndUpdate(todo_id, {notificationToggle: true});
+// //     const savedTodo = await currentUser.save();
+// //     return savedTodo;
+// // }
+
+// // async function turnNotificationsOff(id,todo_id){
+// //     const currentUser = findUserById(id);
+// //     currentUser.todoItems.findByIdAndUpdate(todo_id, {notificationToggle: false});
+// //     const savedTodo = await currentUser.save();
+// //     return savedTodo;
+// // }
+
+// // async function changeCategory(id, todo_id, new_category){
+// //     // maybe a check to see if the category exists?
+// //     const currentUser = findUserById(id);
+// //     currentUser.findByIdAndUpdate(todo_id, {category: new_category});
+// //     const savedTodo = await currentUser.save();
+// //     return savedTodo;
+// // }
 
 
-// // User Services
+// // // User Services
 
 
 
-// async function deleteUser(id) {
-//     return await userModel.findByIdAndDelete(id);
-// }
+// // async function deleteUser(id) {
+// //     return await userModel.findByIdAndDelete(id);
+// // }
 
 exports.addTodo = addTodo;
 exports.findUserById = findUserById;
@@ -255,17 +224,16 @@ exports.addUser = addUser;
 exports.getUserByUsername = getUserByUsername;
 exports.findUserById = findUserById;
 exports.findTodosByCategory = findTodosByCategory;
-exports.markCompleted = markCompleted;
-exports.markUncomplete = markUncomplete;
+// exports.markCompleted = markCompleted;
+// exports.markUncomplete = markUncomplete;
 // exports.removeCompleted = removeCompleted;
 // exports.changeDescription = changeDescription;
 // exports.changeTitle = changeTitle;
 // exports.turnNotificationsOn = turnNotificationsOn;
 // exports.turnNotificationsOff = turnNotificationsOff;
 // exports.changeCategory = changeCategory;
-exports.getUserSettings = getUserSettings;
+// exports.getUserSettings = getUserSettings;
 exports.getTodos = getTodos;
-exports.findTodosByCompleted = findTodosByCompleted;
-exports.getUserCategories = getUserCategories;
+// exports.findTodosByCompleted = findTodosByCompleted;
 exports.getCategories = getCategories;
 exports.addCategory = addCategory;
